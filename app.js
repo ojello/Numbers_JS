020348a83f44b38004c6948c8bb0a7946424590e
Kist <kbear.swe@gmail.com>
2022-01-28 18:47:21 -0600

    push

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// // manipulated the class message
+// document.querySelector('.message').textContent = '🥳 Correct Number'
+
+// // selecting the class number and score
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 18;
+
+
+// // Input element - to get the value we use the value property
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 23;
+
+
+document.querySelector('.check').addEventListener('')
